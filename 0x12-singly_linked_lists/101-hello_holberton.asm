3db7a2159441b612877b16ddf06b64c2cbb75729
DAVID <macajohn.mj@gmail.com>
2022-08-11 12:50:29 -0700

    program in assembly that prints Hello, Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,27 +1,9 @@
-; ----------------------------------------------------------------------------------------
-
-; Writes "Hello, World" to the console using only system calls. Runs on 64-bit Linux only.
-
-; To assemble and run:
-
-;
-
-;     nasm -felf64 hello.asm && ld hello.o && ./a.out
-
-; ----------------------------------------------------------------------------------------
-
-
-
-          global    _start
-	  section   .text
-	  _start:   mov       rax, 1                  ; system call for write
-		              mov       rdi, 1                  ; file handle 1 is stdout
-			      mov       rsi, message            ; address of string to output
-			      mov       rdx, 13                 ; number of bytes
-			      syscall                           ; invoke operating system to do the write
-			      mov       rax, 60                 ; system call for exit
-			      xor       rdi, rdi                ; exit code 0
-			      syscall                           ; invoke operating system to exit
-			      
-			      section   .data
-	        message:  db        "Hello, Holberton", 10      ; note the newline at the end
+          global    main
+          extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
